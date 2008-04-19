73eb18f4bbed8a0091e645820f37cd1419ed99f2
serge.vakulenko <serge.vakulenko@8b245613-3e4b-0410-9dff-3d12a510563e>
2008-04-19 18:43:26 +0000

    2008-04-17 13:18  vak
    	* ChangeLog, src/amiga.c, src/ibmpc.c, src/main.c, src/mfm.h:
    	  Сделана запись индесного маркера.
    
    2008-04-16 20:04  vak
    
    	* src/: ibmpc.c, main.c, mfm.c, mfm.h: Исправлена
    	  запись маркеров A1: теперь с
    	  нарушением кодирования, как
    	  положено.
    
    2008-04-16 16:23  vak
    
    	* AUTHORS, ChangeLog, src/ibmpc.c, src/main.c, src/mfm.c,
    	  src/mfm.h: Добавлен режим --dump - выдача
    	  сырого битового потока.  Улучшена
    	  печать отладочной информации.
    
    2008-04-15 18:50  vak
    
    	* ChangeLog, src/amiga.c, src/ibmpc.c: Добавлены
    	  комментарии.
    
    2008-04-15 17:56  vak
    
    	* ChangeLog, src/amiga.c, src/ibmpc.c, src/main.c, src/mfm.c,
    	  src/mfm.h, src/raw.c: Реализовано создание
    	  MFM-файлов форматов IBM PC и БК-0010.
    
    2008-04-14 22:23  vak
    
    	* src/: amiga.c, ibmpc.c, main.c, mfm.h: Добавлен
    	  режим выдачи информации о формате
    	  MFM-файла.
    
    2008-04-11 12:40  vak
    
    	* ChangeLog, src/Makefile.am, src/Makefile.in, src/amiga.c,
    	  src/ibmpc.c, src/mfm.c, src/raw.c, src/read.c, src/write.c:
    	  Исправлено декодирование блока
    	  данных для amiga.  Более устойчиво
    	  распознавание ibmpc/amiga.  Добавлена
    	  проверка номеров дорожек в каждом
    	  секторе.
    
    2008-04-11 00:54  vak
    
    	* ChangeLog, src/ibmpc.c: Сделана проверка
    	  контрольной суммы для формата IBM PC.
    
    2008-04-10 22:16  vak
    
    	* ChangeLog, src/amiga.c, src/ibmpc.c, src/main.c, src/mfm.h,
    	  src/read.c: Сделана проверка
    	  контрольной суммы для формата Amiga.
    
    2008-04-10 21:30  vak
    
    	* configure, configure.ac, src/Makefile.am, src/Makefile.in,
    	  src/amiga.c, src/ibmpc.c, src/main.c, src/mfm.h, src/mfmdisk.c,
    	  src/read.c, src/write.c: Добавлена поддержка
    	  формата Amiga.
    
    2008-04-10 18:15  vak
    
    	* AUTHORS, COPYING, ChangeLog, INSTALL, Makefile.am, Makefile.in,
    	  NEWS, README, aclocal.m4, config.h.in, configure, configure.ac,
    	  depcomp, install-sh, missing, src/Makefile.am, src/Makefile.in,
    	  src/mfmdisk.c: Первая версия. Умеет
    	  извлекать образы флопов 720к и 800к из
    	  файлов MFM.

---
+++ trunk/src/mfm.h
@@ -0,0 +1,61 @@
+#define MAXTRACK	160
+#define MAXSECT		11
+#define SECTSZ		512
+
+#define INDEX_GAP	42	/* before first sector */
+#define DATA_GAP	22	/* between sector mark and data */
+#define SECTOR_GAP_9	80	/* 720k, 9 sectors per track */
+#define SECTOR_GAP_10	46	/* 800k, 10 sectors per track */
+
+typedef struct {
+	int ntracks;			/* 80 или 160 */
+	int nsectors_per_track;		/* 9..11 */
+	unsigned char block [MAXTRACK] [MAXSECT] [SECTSZ];
+} mfm_disk_t;
+
+typedef struct {
+	FILE *fd;
+	int track;			/* 0..159 */
+	int halfbit;			/* 0..102400 */
+	int byte;
+} mfm_reader_t;
+
+typedef struct {
+	FILE *fd;
+	int last;
+	int halfbit;			/* 0..102400 */
+	int byte;
+} mfm_writer_t;
+
+FILE *mfm_err;
+int mfm_verbose;
+int mfm_gap_byte;
+int mfm_index_gap;
+int mfm_sector_gap;
+int mfm_data_gap;
+
+void mfm_read_seek (mfm_reader_t *reader, FILE *fin, int t);
+int mfm_read_halfbit (mfm_reader_t *reader);
+int mfm_read_bit (mfm_reader_t *reader);
+int mfm_read_byte (mfm_reader_t *reader);
+void mfm_dump (FILE *fin, int ntracks);
+
+void mfm_write_reset (mfm_writer_t *writer, FILE *fout);
+void mfm_write_halfbit (mfm_writer_t *writer, int val);
+void mfm_write_bit (mfm_writer_t *writer, int val);
+void mfm_write (mfm_writer_t *writer, unsigned char *data, int bytes);
+void mfm_write_byte (mfm_writer_t *writer, int val);
+void mfm_write_gap (mfm_writer_t *writer, int nbytes, int val);
+void mfm_fill_track (mfm_writer_t *writer, int val);
+
+void mfm_analyze_ibmpc (FILE *fin, int ntracks);
+void mfm_read_ibmpc (mfm_disk_t *d, FILE *fin, int ntracks);
+void mfm_write_ibmpc (mfm_disk_t *d, FILE *fout, int skip_index_mark);
+
+int mfm_detect_amiga (FILE *fin);
+void mfm_analyze_amiga (FILE *fin, int ntracks);
+void mfm_read_amiga (mfm_disk_t *d, FILE *fin, int ntracks);
+void mfm_write_amiga (mfm_disk_t *d, FILE *fout);
+
+void mfm_read_raw (mfm_disk_t *d, FILE *fin, int nsectors_per_track);
+void mfm_write_raw (mfm_disk_t *d, FILE *fout);
